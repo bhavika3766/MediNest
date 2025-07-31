c04b6a7112a3062a657e862c8394102a66a30034
unknown <bhavika@greensoftsolutions.co.in>
2025-07-31 11:44:44 +0530

    UI changes to dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -19,12 +19,19 @@ const Dashboard = () => {
   return (
     <div className='bg-gradient-to-b  from-emerald-200 gap-x-2 to-blue-300 min-h-screen w-full'>
       <Nav/>
-      
-         {/*<ProductList products={ProductData} />*/}
-      <ul>{apiData.map(Data=>(<li key={Data.id}>{Data.Prodname}— ₹{Data.price}-<img src={Data.image}/></li>)
+      <div >
+         
+      <ul className='grid grid-cols-1 sm:grid-cols-2  md:grid-cols-4 gap-7  items-center justify-center p-6 ml-15'>{apiData.map(Data=>(<li key={Data.id}>
+        <img src={Data.image} className="outline-2 outline-gray-500 hover:drop-shadow-2xl hover:scale-105 hover:rounded-xl  " />
+       <h3 className='text-xl font-semibold mt-2 '>{Data.Prodname}</h3>
+       <h5>Price: Rs.{Data.price}</h5>
+       
+       <button onClick={Data.addToCart} className='hover:font-semibold cursor-pointer mb-2'>Add To Cart</button>
+      </li>)
 
       
       )}</ul>
+      </div>
      
     
 
